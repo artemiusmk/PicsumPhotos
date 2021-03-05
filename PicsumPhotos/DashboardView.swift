0665e15921a820e79d565dcb78797dee450d9ff8
Artem.Makhovyk <artem.makhovyk@ip-192-168-0-167.eu-central-1.compute.internal>
2021-03-05 13:10:37 +0200

    Added author to list item

--- PicsumPhotos/DashboardView.swift
+++ PicsumPhotos/DashboardView.swift
@@ -13,13 +13,37 @@ struct DashboardView: View {
     var viewModel = DashboardViewModel()
     
     var body: some View {
-        List(viewModel.items) {
-            KFImage($0.displayUrl)
+        List(viewModel.items) { item in
+            KFImage(item.displayUrl)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
+                .cornerRadius(8.0)
+                .overlay(
+                    ImageOverlay(author: item.author),
+                    alignment: .bottomLeading
+                )
         }
     }
 }
+
+struct ImageOverlay: View {
+    
+    let author: String?
+    
+    var body: some View {
+        ZStack {
+            Text(author ?? "")
+                .font(.callout)
+                .padding([.top, .bottom], 6.0)
+                .padding([.leading, .trailing], 12.0)
+                .foregroundColor(.white)
+        }.background(Color.black)
+        .opacity(0.7)
+        .cornerRadius(16.0)
+        .padding(10)
+    }
+}
+
 
 struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
